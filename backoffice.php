21ca9d89f2b8116c8bb026cfa235a94a249f033e
nygmasx <103135929+nygmasx@users.noreply.github.com>
2023-03-20 22:12:22 +0100

    Add files via upload

---
+++ backoffice.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+	<title>Administrateurs -- CSE Lycée Saint Vincent</title>
+</head>
+<body>
+
+	<nav class="navbar">
+		<div class="max-width">
+			<ul class="menu">
+				<li><a href="">Accueil</a></li>
+				<li><a href="partenariats.php">Partenariats</a></li>
+				<li><a href="">Billeterie</a></li>
+				<li><a href="">Contact</a></li>
+			</ul>
+			</div>
+		</div>
+		<img class="logo" src="utilisateur.png" alt="user">
+	</nav>
+
+	<main>
+		<div class="home">
+			<h1>Espace administrateurs</h1>
+
+			<div class="boardcard">
+				<h2>Bonjour, <span style="color: #B6A886;">Admin</span></h2> <br><br>
+				<h3>Rôle : </h3>
+				<br>
+				<h3>Dernière connexion : </h3>
+			</div>
+		</div>
+	</main>
+
+
+
+</body>
+</html>
---
+++ partenariats.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="stylez.css">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+	<title>Partenariats | Lycée Saint Vincent</title>
+</head>
+<body>
+
+
+	<nav class="navbar">
+		<div class="max-width">
+			<ul class="menu">
+				<li><a href="backoffice.php">Accueil</a></li>
+				<li><a href="partenariats.php">Partenariats</a></li>
+				<li><a href="">Billeterie</a></li>
+				<li><a href="">Contact</a></li>
+			</ul>
+			</div>
+		</div>
+		<img class="logo" src="utilisateur.png" alt="user">
+	</nav>
+
+
+	<div class="content">
+		<h1>Liste des partenaires</h1>
+
+		<table class="table">
+		<thead>
+			<tr class="table-primary">
+				<th>Nom</th>
+				<th>Texte présentation</th>
+				<th>Lien</th>
+				<th>Action</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td><b>Nodevo</b></td>
+				<td>Découvrez les remises</td>
+				<td><a class="link-primary" href="https://www.chanel.com/fr/parfums/">Découvrir</a></td>
+				<td><button class="modif"><a href="">Modifier</a></button><button class="supp"><a href="">Supprimer</a></button></td>
+			</tr>
+			<tr>
+				<td><b>MozartsDuWeb</b></td>
+				<td>Offres exceptionnelles</td>
+				<td><a class="link-primary" href="https://www.peugeot.fr">Découvrir</a></td>
+				<td><button class="modif"><a href="">Modifier</a></button><button class="supp"><a href="">Supprimer</a></button></td>
+			</tr>
+			<tr>
+				<td><b>MentalWorks</b></td>
+				<td>Offres exceptionnelles sur vos places de cinéma</td>
+				<td><a class="link-primary" href="https://www.pathe.com">Découvrir</a></td>
+				<td><button class="modif"><a href="">Modifier</a></button><button class="supp"><a href="">Supprimer</a></button></td>
+			</tr>
+		</tbody>
+	</table>
+	</div>
+
+
+</body>
+</html>
