a75e899fd49946ed791ff80f6a3e93a848b40228
PeruchoRafael <124828121+PeruchoRafael@users.noreply.github.com>
2023-04-03 15:24:49 +0200

    Page d'accueil fin sprint 1

---
+++ index.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+
+<head>
+  <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>Lycée Saint-Vincent</title>
+  <script defer src='index.js'></script>
+
+</head>
+
+<body>
+  <!-- Header de la page d'accueil -->
+  <header>
+    <div class="fond_gris">
+      <div class="fond_logo">
+        <img src="img/logosv.png" alt="Logo du lycée Saint Vincent">
+
+      </div>
+      </section>
+      <section class="fond_bleu">
+        <label class="menu__btn" for="menu__toggle">
+          <span></span>
+        </label>
+
+        <!-- Création des éléments de la navbar -->
+        <nav>
+          <ul class="navbar_elements">
+            <a href="index.php" class="active">
+              <li id="accueil">Accueil</li>
+            </a>
+            <a href="partenariat.php">
+              <li id="partenariat">Partenariat</li>
+            </a>
+            <a href="#">
+              <li id="billeterie">Billeterie</li>
+            </a>
+            <a href="#">
+              <li id="contact">Contact</li>
+            </a>
+          </ul>
+        </nav>
+
+      </section>
+  </header>
+
+  <!-- Création de la section info -->
+
+  <section class="info">
+    <div class="homelogo">
+      <img src="img/page-daccueil.png" alt="icone d'accueil">
+    </div>
+    <div class="arrow">
+      <img src="img/right-arrow.png" alt="icone de chevron">
+    </div>
+    <h3> Accueil </h3>
+
+    <!-- Création des accès rapides -->
+
+    <div class="titre1">
+      <h2>Accès rapide</h2>
+    </div>
+
+    <div class="raccourci1">
+      <img src="img/right-arrow.png" alt="icone de chevron">
+      <p>Offres/Billeterie</p>
+    </div>
+
+    <div class="raccourci2">
+      <img src="img/right-arrow.png" alt="icone de chevron">
+      <p>Nous contacter</p>
+    </div>
+
+    <!-- Création de la section informations de contact -->
+
+    <div class="titre2">
+      <h2>Informations de contact</h2>
+    </div>
+    <div class="info1">
+      <img src="img/right-arrow.png" alt="icone de chevron">
+      <p>Par téléphone : <span>+33303030303</span></p>
+    </div>
+
+    <div class="info2">
+      <img src="img/right-arrow.png" alt="icone de chevron">
+      <p>Par email : <span>cse@lyceestvincent.fr</span>
+      <p>
+    </div>
+
+    <div class="info3">
+      <img src="img/right-arrow.png" alt="icone de chevron">
+      <p>Au lycée : <span> Bureau du CSE <br>(1er étage bâtiment Saint-Vincent)</span></p>
+    </div>
+
+    <!-- Création de la section partenaires -->
+
+    <div class="titre3">
+      <h2>Nos partenaires</h2>
+    </div>
+    <div class="partenaires">
+      <div class="leftarrow">
+        <img src="img/left-arrow.png" alt="icone de flèche de gauche">
+      </div>
+      <img src="img/leonidas.png" alt="logo de Leonidas">
+      <div class="rightarrow">
+        <img src="img/right-arrow.png" alt="icone de flèche de droite">
+      </div>
+      <p>Découvrez tous nos partenaires</p>
+    </div>
+  </section>
+  
+  <!-- Création de la page de droite -->
+  
+  <section class="partie-droite">
+    <div class="bienvenue">
+      <h2>CSE Lycée Saint Vincent</h2>
+      <p>Nous vous souhaitons la bienvenue sur le site du comité social et économique du lycée Saint-Vincent à Senlis.
+        <br>
+        Découvrez l’<span>équipe</span> et le <span>rôle</span> et missions de votre CSE.</p>
+    </div>
+
+    <!-- Création de la section offre billeterie -->
+
+    <div class="offre-billeterie">
+      <h1>Dernières offres de la billeterie</h1>
+      <div class="offre1">
+        <div class="hautdepage">
+          <div class="btn-offre">
+            <p>OFFRE</p>
+          </div>
+          <div class="date-offre">
+            <p>Publié le 11 décembre 2022</p>
+          </div>
+        </div>
+        <div class="contenu-offre">
+          <p>Profitez de -20% sur une sélection de parfum, en partenariat avec l'enseigne Nocibé de Senlis.Merci de vous
+            rendre au bureau du CSE pour pouvor passer commande</p>
+        </div>
+        <div class="en-savoir-plus">
+          <p>EN SAVOIR PLUS</p>
+          <img src="img/right-arrow.png" alt="image de chevron">
+        </div>
+      </div>
+      
+      <div class="offre2">
+        <div class="hautdepage">
+          <div class="btn-offre">
+            <p>OFFRE</p>
+          </div>
+          <div class="date-offre">
+            <p>Publié le 09 décembre 2022</p>
+          </div>
+        </div>
+        <div class="contenu-offre">
+          <p>Achetez dès à présent votre sapin de noël en profitant du partenariat entre le lycée Saint-Vincent 
+            et l'association des scouts De l'oise <br>Prix commun à tous : 30€ le petit sapin, 40€ le grand.</p>
+        </div>
+        <div class="en-savoir-plus2">
+          <p>EN SAVOIR PLUS</p>
+          <img src="img/right-arrow.png" alt="image de chevron">
+        </div>
+      </div>
+      
+      <div class="offre3">
+        <div class="hautdepage">
+          <div class="btn-offre">
+            <p>OFFRE</p>
+          </div>
+          <div class="date-offre">
+            <p>Publié le 10 novembre 2022 - Jusqu'au 30/01/2023</p>
+          </div>
+        </div>
+        <div class="contenu-offre">
+          <p>Offre négociée au près de Léonidas, profitez de - 10% toute l'année sur l'ensemble des chocolats dans la boutique de Senlis
+            <br>Famille nombreuse (5 enfants et plus) : -5% supplémentaire</p>
+        </div>
+        <div class="en-savoir-plus3">
+          <p>EN SAVOIR PLUS</p>
+          <img src="img/right-arrow.png" alt="image de chevron">
+        </div>
+      </div>
+    </div>
+  </section>
+  
+
+
+
+</body>
+
+</html>
+<main>
+
+</main>
+<footer>
+</footer>
+</body>
+
+</html>
