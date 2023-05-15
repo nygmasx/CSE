f99586bd986c1dba3dcb780819bf708ffa0cbea7
nourdyne <126764347+nourdyne@users.noreply.github.com>
2023-05-15 12:09:13 +0200

    Add files via upload

---
+++ partenariat.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+	<title></title>
+</head>
+<body>
+	<header>
+    <section class="fond_gris">
+        <div class="fond_logo">
+            <div class="logo_st_vincent"> <img src="logo-daccueil.png"> </div>
+        </div>
+    </section>
+        <section class="fond_bleu">
+            <div class="liste">
+    
+    <!-- Création de la navbar -->
+
+                  <ul class="navbar_elements">
+                    <a href="index.php"><li id="accueil">Accueil</a></li>
+                      <a href="partenariat.php"><li class="active" id="partenariat">Partenariat</a></li>
+                        <a href="#"><li id="billeterie">Billeterie</a></li>
+                          <a href="#"><li id="contact">Contact</a></li>
+                  </ul>
+                </div>
+        </section>
+	</header>
+
+  <!-- Création de la section info -->
+
+
+
+  <!--autre-->
+
+<section class="info">
+    <div class="accueil">
+        <div class="homelogo">
+        <img src="img/page-daccueil.png" alt="icone d'accueil">
+        </div>
+        <div class="arrow">
+            <img src="img/chevron.png" alt="icone de chevron">
+        </div>
+        <h3> Accueil </h3>
+    </div>
+
+    <!-- Création des accès rapides -->
+
+    <div class="titre1">
+      <h2>Accès rapide</h2>
+    </div>
+
+    <div class="raccourci1">
+      <img src="imgchevron.png" alt="icone de chevron">
+      <p>Offres / Billeterie</p>
+    </div>
+
+    <div class="raccourci2">
+      <img src="imgchevron.png" alt="icone de chevron">
+      <p>Nous contacter</p>
+    </div>
+
+    <!-- Création de la section informations de contact -->
+
+    <div class="titre2">
+      <h2>Informations de contact</h2>
+    </div>
+    <div class="info1">
+      <img src="imgchevron.png" alt="icone de chevron">
+      <p>Par téléphone : <span>+33303030303</span></p>
+    </div>
+
+    <div class="info2">
+      <img src="imgchevron.png" alt="icone de chevron">
+      <p>Par email : <span>cse@lyceestvincent.fr</span>
+      <p>
+    </div>
+
+    <div class="info3">
+      <img src="imgchevron.png" alt="icone de chevron">
+      <p>Au lycée : <span> Bureau du CSE <br>(1er étage bâtiment Saint-Vincent)</span></p>
+    </div>
+
+    <!-- Création de la section partenaires -->
+<section>
+    <div class="titre3">
+      <h2>Nos partenaires</h2>
+    </div>
+    <div class="partenaires">
+      <div class="leftarrow">
+        
+      </div>
+      <img class="racourcileonidas" src="leonidas.jpeg" alt="logo de Leonidas">
+      <div class="rightarrow">
+        
+      </div>
+    </div>
+    <div class="discover">
+        <p>Découvrez tous nos partenaires</p>
+    </div>
+  </section>
+  
+
+      <!-- Création de la page partenariat -->
+
+  <section class="section_droite">
+    <p class="soustitre">Voici nos partenaires !</p>
+    <div class="fondgris">
+      <img class="imgnocibe" src="Logo_Nocibe.png">
+      <p class="nocibe">NOCIBÉ</p>
+      <p class="txtnocibe">
+        Nocibé est en partenariat avec le lycée St Vincent, pour promouvoir les métiers du numérique auprès des étudiants. Ce partenariat offre aux élèves des formations, des stages en entreprise et des événements pour découvrir les dernières<br> innovations technologiques.
+      </p>
+    </div>
+
+    <div class="fondgris">
+     <img class="imgnike" src="sticker-nike.png">
+     <p class="nike">NIKE</p>
+      <p class="txtnike">
+        Nike est une entreprise américaine de sport et de mode. En partenariat avec le lycée St Vincent, Nike encourage l'activité physique et le bien-être des étudiants en leur fournissant des équipements de sport et en organisant des événements sportifs.
+     </p>
+    </div>
+
+    <div class="fondgris">
+     <img class="imgleonidas" src="leonidas.jpeg">
+     <p class="leonidas">LÉONIDAS</p>
+     <p class="txtnocibe">
+       Leonidas crée des sites web personnalisés avec une équipe de  professionnels passionnés. En partenariat avec le lycée, nous offrons des stages pour développer les compétences des étudiants en création de sites web et les préparer pour le monde professionnel.
+     </p>
+    </div>
+
+    <button class="decouvrir-part">
+      <p class="phrasefin">Découvrir tous nos partenaires ></p>
+    </button>
+
+  </section>
+</body>
+</html>
