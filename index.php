4a37c7254216dcb9674d0e3d604484a23c4b3e98
Rafael Perucho <rafael.prc17@gmail.com>
2023-05-23 15:51:03 +0200

    ajout modale

--- index.php
+++ index.php
@@ -5,11 +5,87 @@ $statement = $pdo->prepare($sql);
 $statement->execute();
 $offres = $statement->fetchAll();
 foreach ($offres as $offre) {
-    $query = "SELECT * FROM `offre_image` INNER JOIN `image` ON  `offre_image`.Id_Image = `image`.Id_Image  WHERE `offre_image`.Id_Offre = ?";
-    $get_all_image = $pdo->prepare($query);
-    $get_all_image->execute([$offre['Id_Offre']]);
-    $images = $get_all_image->fetchAll();
+  $query = "SELECT * FROM `offre_image` INNER JOIN `image` ON  `offre_image`.Id_Image = `image`.Id_Image  WHERE `offre_image`.Id_Offre = ?";
+  $get_all_image = $pdo->prepare($query);
+  $get_all_image->execute([$offre['Id_Offre']]);
+  $images = $get_all_image->fetchAll();
 }
+?>
+<?php
+
+if (isset($_GET['Idoffre'])) {
+  $query = "SELECT * FROM `offre_image` INNER JOIN `image` ON  `offre_image`.Id_Image = `image`.Id_Image  WHERE `offre_image`.Id_Offre = ?";
+  $get_all_image = $pdo->prepare($query);
+  $get_all_image->execute([$_GET['Idoffre']]);
+  $images = $get_all_image->fetchAll();
+  $query = "SELECT * FROM offre WHERE Id_Offre = :id ";
+  $get_Info = $pdo->prepare($query);
+  $get_Info->bindParam("id", $_GET['Idoffre']);
+  $get_Info->execute();
+  $offre = $get_Info->fetch();
+
+  $query = "SELECT * FROM `partenaire` WHERE `Id_Partenaire` = ?";
+  $statement = $pdo->prepare($query);
+  $statement->execute([$offre['Id_Partenaire']]);
+  $partenaire = $statement->fetch();
+   
+  ?>
+
+  <div class="modal" id="modal1" style="display:flex !important;">
+    <div class="contenu hidden">
+      <span class="fermer">&times;</span>
+      <h2> <?=$partenaire['Nom_Partenaire']; ?> : <?= $offre['Nom_Offre']; ?> </h2>
+      <div class="contenumodal">
+
+      </div>
+      <?php
+      if (!empty($images)) {
+        
+      ?>
+      <div class="date-offres">
+        <p>Offre valable du <?= $offre['Date_Debut_Offre']; ?> au <?= $offre['Date_Fin_Offre']; ?> </p>
+      </div>
+        <div class="description-offre">
+          <p><?= $offre['Description_Offre'] ?></p>
+        </div>
+            <div class="img-partenaires"> <?php
+            foreach ($images as $img) { ?>
+              <img src="assets/<?php echo $img['Nom_Image']; ?>" <?= count($images) >1 ? "style='width: calc(100% /".count($images).");'" : "" ?>>
+            
+            <?php } ?>
+            
+            </div>
+      <?php
+        
+      }
+      ?>
+    </div>
+  </div>
+  </div>
+
+
+  <script>
+    //Fermer Modal
+    let span = document.querySelector(".fermer");
+    // Get the modal
+    let modal = document.getElementById("modal1");
+
+    span.onclick = function() {
+      modal.style.display = "none";
+      history.pushState(null, null, window.location.href.split("?")[0]);
+    }
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+        history.pushState(null, null, window.location.href.split("?")[0]);
+      }
+    }
+  </script>
+
+
+<?php
+}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -28,28 +104,28 @@ foreach ($offres as $offre) {
 <body>
 
 
-<!-- Ajout de la navbar à la page d'accueil -->
-  
-  <?php 
-    include("navbar.php");
+  <!-- Ajout de la navbar à la page d'accueil -->
+
+  <?php
+  include("navbar.php");
   ?>
 
-<div class="page-accueil">
-    <?php 
-      include("section-gauche.php");
+  <div class="page-accueil">
+    <?php
+    include("section-gauche.php");
     ?>
 
     <?php
-    include ("section-droite.php");
+    include("section-droite.php");
     ?>
-  
-</div>
 
-    <?php
-      include("footer.php");
-    ?>
-  
-  
+  </div>
+
+  <?php
+  include("footer.php");
+  ?>
+
+
 
 
 
--- section-droite.php
+++ section-droite.php
@@ -31,77 +31,49 @@
 
     <div class="offre-billeterie">
       <h1>Dernières offres de la billeterie</h1>
-    
-    <?php
-    $statement = $pdo->prepare($sql);
-    $statement->execute();
-    $offres = $statement->fetchAll();
-    foreach ($offres as $offre) {
-      $query = "SELECT * FROM `offre_image` INNER JOIN `image` ON  `offre_image`.Id_Image = `image`.Id_Image  WHERE `offre_image`.Id_Offre = ?";
-      $get_all_image = $pdo->prepare($query);
-      $get_all_image->execute([$offre['Id_Offre']]);
-      $images = $get_all_image->fetchAll();
-   
 
-    ?>
+      <?php
+      $statement = $pdo->prepare($sql);
+      $statement->execute();
+      $offres = $statement->fetchAll();
+      foreach ($offres as $offre) {
+        $query = "SELECT * FROM `offre_image` INNER JOIN `image` ON  `offre_image`.Id_Image = `image`.Id_Image  WHERE `offre_image`.Id_Offre = ?";
+        $get_all_image = $pdo->prepare($query);
+        $get_all_image->execute([$offre['Id_Offre']]);
+        $images = $get_all_image->fetchAll();
 
-    
-      <div class="offre">
-        <div class="haut-de-page">
-          <div class="btn-offre">
-            <p><?= $offre['Nom_Offre']; ?></p>
-          </div>
-          <div class="date-offre">
-            <p>Publié le <?= $offre['Date_Debut_Offre']; ?></p>
-          </div>
-        </div>
-        <div class="contenu-offre">
-          <p><?= $offre['Description_Offre'] ?></p>
-        </div>
-        <div class="en-savoir-plus" data-target="#modal1">
-          <p><span>EN SAVOIR PLUS</span></p>
-          <img src="assets/img/chevron.png" alt="image de chevron">
 
-          
-          <div class="modal" id="modal1">
-            <div class="contenu hidden">
-              <span class="fermer">&times;</span>
-              <h1>PROJET 1</h1>
-              <div class="contenumodal">
-                <img src="projetalex.png">
-                <p>C'est le premier projet effectué en classe. Ce fut pour moi l'occasion d'expérimenter pour la première fois l'utilisation du langage HTML et CSS. Le but était de reproduire le plus fidèlement possible un exemple de CV. Je m'occupais de la partie où Alex donne ses données personnelles.</p>
-                < </div>
-                  <div class="temps">
-                    <h3>Temps accordé : <span>~ 2 heures</span></h3>
-                  </div>
-                  <div class="techno">
-                    <h3>Technologies utilisées :</h3>
-                    <img src="html.png">
-                    <img src="css.png">
-                  </div>
-              </div>
+      ?>
+
+
+        <div class="offre">
+          <div class="haut-de-page">
+            <div class="btn-offre">
+              <p><?= $offre['Nom_Offre']; ?></p>
+            </div>
+            <div class="date-offre">
+              <p>Offre valable du <?= $offre['Date_Debut_Offre']; ?> au <?= $offre['Date_Fin_Offre']; ?> </p>
             </div>
           </div>
+          <div class="contenu-offre">
+            <p><?= $offre['Description_Offre'] ?></p>
+          </div>
+          <form action="index.php" method="get">
+            <input type="hidden" name="Idoffre" value="<?= $offre['Id_Offre'] ?>">
+            <button type="submit" class="en-savoir-plus" data-target="#modal1">
+              <p><span>EN SAVOIR PLUS</span></p>
+              <img src="assets/img/chevron.png" alt="image de chevron">
+            </button>
+          </form>
         </div>
-<?php } ?>
+
+
+      <?php } ?>
 
   </section>
   </div>
 
-  <script>
-    //Fermer Modal
-    let span = document.getElementsByClassName("fermer")[0];
-    // Get the modal
-    let modal = document.getElementById("modal");
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
-  </script>
+
 </body>
 
 </html>
